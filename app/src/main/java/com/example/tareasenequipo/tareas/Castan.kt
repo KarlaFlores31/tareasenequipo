7a9315771b1789cb39c7a8338d4b68471e44cb16
sofiavsc <ana.soqui@iest.edu.mx>
2026-02-04 10:03:49 -0600

    Texto subtitulo

--- app/src/main/java/com/example/tareasenequipo/tareas/Castan.kt
+++ app/src/main/java/com/example/tareasenequipo/tareas/Castan.kt
@@ -65,7 +65,7 @@ fun Multiservicios (){
             Box(modifier = Modifier.fillMaxWidth(),
                 contentAlignment = Alignment.Center)
             {
-                Text(text = "128 seguidores · 1 seguidos",
+                Text(text = "128 seguidores",
                     color = Color.White, fontSize = 14.sp, fontWeight = FontWeight.Medium,
                     lineHeight = 30.sp)
             }
